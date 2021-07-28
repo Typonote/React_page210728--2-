0ccea7b24cc1c37e78a3bcdf65be102d74c6a5c9
[2기-1반] 김진경 <kimradium@naver.com>
2021-07-28 13:41:58 +0900

    7번째: NavTag변경(매개변수와 배열을 생성)

--- src/App.js
+++ src/App.js
@@ -2,48 +2,60 @@ import logo from './logo.svg';
 import './App.css';
 
 // HeaderTag 컴포넌트 생성
-function HeaderTag(props){
+function HeaderTag(props) {
   return (
     <header>
-        <h1>
-          <a href="index.html">{props.title}</a>
-        </h1>
-      </header>
+      <h1>
+        <a href="index.html">{props.title}</a>
+      </h1>
+    </header>
   );
 }
 
 // NavTag 컴포넌트 생성
-function NavTag(){
-  return(
+function NavTag(props) {
+
+  console.log('props', props.data);
+  var d = props.data;
+  var lis = [];
+
+  for (var i = 0; i < d.length; i++) {
+    console.log(i, d[i]);
+    lis.push(<li>{d[i].title}</li>)
+  }
+
+  return (
     <nav>
-        <ul>
-          <li><a href="1.html">html</a></li>
-          <li><a href="2.html">css</a></li>
-        </ul>
+      <ul>
+        {lis}
+      </ul>
     </nav>
   );
 }
 
 // ArticleTag 컴포넌트 생성
-function ArticleTag(props){
-  console.log('props',props.title);
-  return(
+function ArticleTag(props) {
+  return (
     <article>
-        <h2 style={{color:'red', fontSize: '100px'}}>{props.title}</h2>
-        {props.description}
+      <h2 style={{ color: 'red' }}>{props.title}</h2>
+      {props.description}
     </article>
   );
 }
 
 function App() {
+
+  // topics배열생성 => NavTag에서 사용
+  var topics = [
+    { id: 1, title: 'html', description: 'html is....' },
+    { id: 2, title: 'css', description: 'css is....' }
+  ]
+
   return (
     <div className="App">
-      <HeaderTag title="I love WEB"/>
-      <NavTag />
-      <ArticleTag title="Welcome" description="Hello, WEB"/>
-      <ArticleTag title="Hi" description="Hello, REACT"/>
-      <ArticleTag title="Hi2" description="Hello, REACT"/>
-      <ArticleTag title="Hi3" description="Hello, REACT"/>
+      <HeaderTag title="I love WEB" />
+      <NavTag data={topics} />
+      <ArticleTag title="Welcome" description="Hello, WEB" />
     </div>
   );
 }
