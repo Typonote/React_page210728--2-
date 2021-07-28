c372c512064037b3de261b4fb85639cc71ad40ab
[2기-1반] 김진경 <kimradium@naver.com>
2021-07-28 15:00:48 +0900

    10번째: Header와 Nav의 Handler 분리 + 간단한 mode적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 // Header 컴포넌트 생성
 function Header(props) {
-  console.log('Header', props)
-  function onClickHandler() {
+  // console.log('Header', props)
+  function onClickHandler(e) {
+    // a 태그가 어딘가의 링크로 이동하므로 그 동작을 취소. a가 아닌 다른 태그에서의 onClick 이벤트에는 preventDefault가 필요없다.=> a태그의 원래 갖고있던 기능을 차단하고 내가 작성한 것만 구현
+    e.preventDefault();
     props.onChangeMode();
   }
   return (
@@ -19,11 +22,16 @@ function Header(props) {
 // NavTag 컴포넌트 생성
 function NavTag(props) {
 
+  function onClickHandler(e) {
+    e.preventDefault();
+    props.onChangeMode();
+  }
+
   var d = props.data;
   var lis = [];
 
   for (var i = 0; i < d.length; i++) {
-    lis.push(<li key={d[i].id}><a href={d[i].id}>{d[i].title}</a></li>);
+    lis.push(<li key={d[i].id}><a href={d[i].id} onClick={onClickHandler}>{d[i].title}</a></li>);
     // key를 사용하는 이유: 렌더링 시 좀 더 빨라진다.
   }
 
@@ -48,22 +56,46 @@ function ArticleTag(props) {
 
 function App() {
 
+  var [mode, setMode] = useState('WELCOME');
+  console.log('mode', mode);
+
+
+
   // topics배열생성 => NavTag에서 사용
   var topics = [
     { id: 1, title: 'html', description: 'html is....' },
     { id: 2, title: 'css', description: 'css is....' }
   ]
 
-  function onChangeModeHandler() {
+  function onChangeHeaderHandler() {
     // mode에 맞는 기능이 실행되도록 설정하는 핸들러
-    console.log('onChangeMode!!!!')
+    console.log('Header!!!!');
+    setMode('WELCOME');
   }
+
+  function onChangeNavHandler() {
+    // mode에 맞는 기능이 실행되도록 설정하는 핸들러
+    console.log('Nav!!!!');
+    setMode('READ');
+  }
+
+  var title;
+  var desc;
+
+  if (mode === 'WELCOME') {
+    title = 'Welcome';
+    desc = 'Hello, React'
+  } else if (mode === 'READ') {
+    title = 'Read Mode';
+    desc = 'Hello, Read';
+  }
+
 
   return (
     <div className="App">
-      <Header title="I love WEB" onChangeMode={onChangeModeHandler} />
-      <NavTag data={topics} onChangeMode={onChangeModeHandler} />
-      <ArticleTag title="Welcome" description="Hello, WEB" />
+      <Header title="I love WEB" onChangeMode={onChangeHeaderHandler} />
+      <NavTag data={topics} onChangeMode={onChangeNavHandler} />
+      <ArticleTag title={title} description={desc} />
     </div>
   );
 }
