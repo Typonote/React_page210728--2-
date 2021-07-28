a75e61030a16b06093f6d1a218f0f807c9742a6b
[2기-1반] 김진경 <kimradium@naver.com>
2021-07-28 11:40:22 +0900

    2번째: HeaderTag 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function HeaderTag(){
+  return (
+    <header>
+        <h1>
+          <a href="index.html">WEB</a>
+        </h1>
+      </header>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <HeaderTag />
+      <nav>
+        <ul>
+          <li><a href="1.html">html</a></li>
+          <li><a href="2.html">css</a></li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
